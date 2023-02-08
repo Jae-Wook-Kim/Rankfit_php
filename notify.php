ff9033668b565f53c86dfd39ae3690f53f4f37e9
Desktop <8370kjw@naver.com>
2023-02-08 23:11:13 +0900

    230208_2311

---
+++ notify.php
@@ -0,0 +1,26 @@
+<?php
+    require("dbset.php");
+    $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $Index = NULL;
+    $nickname = isset($_POST["nickname"]) ? $_POST["nickname"] : "";
+    $reason = isset($_POST["reason"]) ? $_POST["reason"] : "";
+    $userID = isset($_POST["userID"]) ? $_POST["userID"] : "";
+    $date = isset($_POST["date"]) ? $_POST["date"] : "";
+
+    $response = false;
+
+    $sql5 = "SELECT userID FROM userTBL WHERE userNickname = '$nickname'";
+    $result5 = mysqli_query($con,$sql5);
+    $row5 = mysqli_fetch_array($result5);
+    $ID = $row5['userID'];
+
+    $statement = mysqli_prepare($con, "INSERT INTO notify VALUES (?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "sssss", $Index, $ID, $reason, $userID, $date);
+    mysqli_stmt_execute($statement);
+
+    $response = true;
+
+    echo json_encode($response);
+?>
