dbaffb6251cfb49560382931d0a37e22c1ce9a8a
Desktop <8370kjw@naver.com>
2023-02-12 16:00:12 +0900

    2302312_1600

--- uploadimages.php
+++ uploadimages.php
@@ -1,2 +1,15 @@
 <?php
+    require("dbset.php");
+    $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $picture = $_POST["imageStr"];
+    $floder_path = "images/" . time() . ".png";
+    $data = str_replace('data:image/png;base64,', '', $picture);
+    $data = str_replace(' ', '+', $data);
+    $data = base64_decode($data);
+    file_put_contents($floder_path, $data);
+    $sql = "INSERT INTO images (path) VALUES ('$floder_path')";
+    mysqli_query($con, $sql);
+    echo "Image has been uploaded"
 ?>
