0dc433ff394b5d757e90e52929782ecb80c16b9a
egoing <8370kjw@naver.com>
2023-02-16 19:12:48 +0900

    230216_1912

--- imageDown.php
+++ imageDown.php
@@ -3,11 +3,17 @@
     $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
     mysqli_query($con,'SET NAMES utf8');
 
-    $image_name = isset($_POST["image_name"]) ? $_POST["image_name"] : "";
+    $sql2 = "SELECT userNickname FROM userTBL WHERE userID = '$ID'";
+    $result2 = mysqli_query($con,$sql2);
+    $row2 = mysqli_fetch_array($result2);
+    
+    $image_name = $row2['userNickname'];
+
+    // $image_name = isset($_POST["image_name"]) ? $_POST["image_name"] : "";
 
     if($image_name != "") {
 
-        $sql = "SELECT image_name FROM images WHERE image_name = '$image_name'";
+        $sql = "SELECT image_name FROM images WHERE image_name LIKE '$image_name%'";
         $result = mysqli_query($con,$sql);
 
         $row = mysqli_fetch_array($result);
