fd9c6d03cf34cd7d7fd7dfe419dc2cf07c64761d
MyNotebook <8370kjw@naver.com>
2023-02-22 23:04:04 +0900

    230222_2303

--- weekEXrank.php
+++ weekEXrank.php
@@ -3,10 +3,26 @@
     $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
     mysqli_query($con,'SET NAMES utf8');
 
-    $sql = "SELECT * FROM running";
+    $sql = "SELECT userExercise, COUNT(*) cnt FROM anaerobicTBL WHERE userState = '1' GROUP BY userEXercise ORDER BY cnt DESC";
     $result = mysqli_query($con,$sql);
-    $tmp = mysqli_num_rows($result);
+    // $tmp = mysqli_num_rows($result);
     
-    echo $tmp;
+    $test10 = array();
+    $test11 = [];
+    $response3 = array();
+    $response2 = array();
+    (int)$count = 0;
+
+    while ($row = mysqli_fetch_array($result)) {
+        $count += 1;
+        $response3["Exercise"] = $row['userExercise'];
+        $response3["Rank"] = (string)$count;
+
+        $test11[] = $response3;
+    }
+    $test10["All"] = $test11;
+
+    $json = json_encode($test10, JSON_UNESCAPED_UNICODE);
+    echo $json;
     mysqli_close($con);
 ?>
