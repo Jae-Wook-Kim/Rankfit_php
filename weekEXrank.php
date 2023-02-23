9431d073ef668542199c0f8523629617cdb154f7
Desktop <8370kjw@naver.com>
2023-02-23 19:27:45 +0900

    230223_1927

--- weekEXrank.php
+++ weekEXrank.php
@@ -3,9 +3,15 @@
     $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
     mysqli_query($con,'SET NAMES utf8');
 
-    $sql = "SELECT userExercise, COUNT(*) cnt FROM anaerobicTBL WHERE userState = '1' GROUP BY userEXercise ORDER BY cnt DESC";
-    $result = mysqli_query($con,$sql);
+    // $sql3 = "SELECT aerobicTBL a inner join (SELECT userExercise, COUNT(*) cnt FROM anaerobicTBL WHERE userState = '1' GROUP BY userEXercise ORDER BY cnt DESC) b on b.userID = a.userID SET a.AgeRank = b.Ranking";
+    // $result3 = mysqli_query($con,$sql3);
+
+    // $sql = "SELECT userExercise, COUNT(*) cnt FROM anaerobicTBL WHERE userState = '1' GROUP BY userEXercise ORDER BY cnt DESC";
+    // $result = mysqli_query($con,$sql);
     // $tmp = mysqli_num_rows($result);
+
+    $sql = "(SELECT userExercise, COUNT(*) cnt FROM anaerobicTBL WHERE userState = '1' GROUP BY userEXercise) UNION (SELECT userExercise, COUNT(*) cnt FROM aerobicTBL WHERE userState = '1' GROUP BY userEXercise) ORDER BY cnt DESC";
+    $result = mysqli_query($con,$sql);
     
     $test10 = array();
     $test11 = [];
