7f566d650e4badfbbb3e7f8c8fcfba7c98814e21
Desktop <8370kjw@naver.com>
2023-02-26 12:30:19 +0900

    230226_1230

--- notice.php
+++ notice.php
@@ -19,6 +19,7 @@
     }
     $test10["Notice"] = $test11;
 
+    header('Content-Type: application/json; charset=utf-8');
     $json = json_encode($test10, JSON_UNESCAPED_UNICODE);
     echo $json;
     mysqli_close($con);
