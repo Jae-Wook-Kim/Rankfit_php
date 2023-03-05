4d31c66c64fde186fbe6904bde5fad01616dd16a
Desktop <8370kjw@naver.com>
2023-03-05 22:49:46 +0900

    230305_2249

--- runningRank.php
+++ runningRank.php
@@ -3,6 +3,7 @@
     $con = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("MySQL Connection Failed !!");
     mysqli_query($con,'SET NAMES utf8');
 
+    $userID = isset($_POST["userID"]) ? $_POST["userID"] : "";
     $start = isset($_POST["start"]) ? $_POST["start"] : 0;
     $end = isset($_POST["end"]) ? $_POST["end"] : 0;
 
@@ -19,6 +20,10 @@
         $count += 1;
 
         $ID = $row['userID'];
+        if ($userID == $ID) {
+            $response2["My_Ranking"] = (string)$count;
+            $response2["My_Score"] = $row['tmp'];
+        }
         $sql2 = "SELECT userNickname FROM userTBL WHERE userID = '$ID'";
         $result2 = mysqli_query($con,$sql2);
         $row2 = mysqli_fetch_array($result2);
@@ -30,6 +35,7 @@
         $test11[] = $response3;
     }
     $test10["All"] = $test11;
+    $test10["My"] = $response2;
 
     $json = json_encode($test10, JSON_UNESCAPED_UNICODE);
     echo $json;
